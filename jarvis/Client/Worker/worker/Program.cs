6e5e80cdb2fc2ac7d690d56a7ec309030e3825f2
Andreas Willich <sabotageandi@gmail.com>
2012-04-01 19:33:11 +0200

    stupid vs11 did not saved all files ...

--- jarvis/Client/Worker/worker/Program.cs
+++ jarvis/Client/Worker/worker/Program.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿// J.A.R.V.I.S. - Just A Rather Very Intelligent System
+// Copyright (C) 2012 Andreas Willich <sabotageandi@gmail.com>
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
 using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.IO;
@@ -12,14 +28,14 @@ using jarvis.common.dtos.Workflow;
 
 namespace jarvis.client.worker
 {
-    class Program
+    internal class Program
     {
-        static RestClient  _client = new RestClient("http://localhost:5368/Services/WorkflowService.svc");
+        private static readonly RestClient _client = new RestClient("http://localhost:5368/Services/WorkflowService.svc");
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Thread.Sleep(30000);
-            while(true)
+            while (true)
             {
                 if (!Do())
                 {
@@ -35,7 +51,9 @@ namespace jarvis.client.worker
             var restResponse = _client.Execute<RunnedWorkflowDto>(workflowToExecuteRequest);
 
             if (restResponse == null || restResponse.Data == null)
+            {
                 return false;
+            }
 
             ExecuteWorkflow(restResponse.Data);
 
@@ -53,9 +71,12 @@ namespace jarvis.client.worker
 
             foreach (var runnedNextWorkflowStepDto in nextSteps2Execute)
             {
-                var step2Execute = runnedWorkflowDto.WorkflowSteps.Where(ws => ws.Id == runnedNextWorkflowStepDto.NextStepId).SingleOrDefault();
+                var step2Execute =
+                    runnedWorkflowDto.WorkflowSteps.Where(ws => ws.Id == runnedNextWorkflowStepDto.NextStepId).SingleOrDefault();
                 if (step2Execute == null)
+                {
                     continue;
+                }
 
                 ExecuteStep(step2Execute);
             }
@@ -63,7 +84,9 @@ namespace jarvis.client.worker
             foreach (var runnedNextWorkflowStepDto in nextSteps2Execute)
             {
                 if (runnedNextWorkflowStepDto.NextStepId != null)
+                {
                     ExecuteStepsAfterStep(runnedWorkflowDto, runnedNextWorkflowStepDto.NextStepId.Value);
+                }
             }
         }
 
--- jarvis/Server/model/WorkflowLogic.cs
+++ jarvis/Server/model/WorkflowLogic.cs
@@ -1,8 +1,21 @@
-﻿using System;
+﻿// J.A.R.V.I.S. - Just A Rather Very Intelligent System
+// Copyright (C) 2012 Andreas Willich <sabotageandi@gmail.com>
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using jarvis.common.dtos.Workflow;
 using jarvis.server.common.Database;
 using jarvis.server.entities.Workflow;
@@ -17,16 +30,16 @@ namespace jarvis.server.model
 
     public class WorkflowLogic : IWorkflowLogic
     {
-        private readonly IWorkflowQueueRepository _workflowQueueRepository;
+        private readonly IRunnedNextWorkflowStepRepository _runnedNextWorkflowStepRepository;
+        private readonly IRunnedTaskRepository _runnedTaskRepository;
         private readonly IRunnedWorkflowRepository _runnedWorkflowRepository;
         private readonly IRunnedWorkflowStepRepository _runnedWorkflowStepRepository;
-        private readonly IRunnedTaskRepository _runnedTaskRepository;
-        private readonly IRunnedNextWorkflowStepRepository _runnedNextWorkflowStepRepository;
         private readonly ITransactionProvider _transactionProvider;
+        private readonly IWorkflowQueueRepository _workflowQueueRepository;
 
         public WorkflowLogic(IWorkflowQueueRepository workflowQueueRepository, IRunnedWorkflowRepository runnedWorkflowRepository,
-            IRunnedWorkflowStepRepository runnedWorkflowStepRepository, IRunnedTaskRepository runnedTaskRepository,
-            IRunnedNextWorkflowStepRepository runnedNextWorkflowStepRepository, ITransactionProvider transactionProvider)
+                             IRunnedWorkflowStepRepository runnedWorkflowStepRepository, IRunnedTaskRepository runnedTaskRepository,
+                             IRunnedNextWorkflowStepRepository runnedNextWorkflowStepRepository, ITransactionProvider transactionProvider)
         {
             _workflowQueueRepository = workflowQueueRepository;
             _runnedWorkflowRepository = runnedWorkflowRepository;
@@ -41,7 +54,9 @@ namespace jarvis.server.model
             var workflowToExecute = _workflowQueueRepository.GetNextQueuedWorkflowAndSetStarttime();
 
             if (workflowToExecute == null)
+            {
                 return null;
+            }
 
             var runnedWorkflow = CreateRunnedWorkflow(workflowToExecute.DefinedWorkflow);
             workflowToExecute.RunnedWorkflow = runnedWorkflow;
@@ -69,20 +84,21 @@ namespace jarvis.server.model
         private RunnedNextWorkflowStepDto TranslateToDto(RunnedNextWorkflowStep runnedNextWorkflowStep)
         {
             return new RunnedNextWorkflowStepDto()
-                                                   {
-                                                       Id = runnedNextWorkflowStep.Id,
-                                                       NextStepId = runnedNextWorkflowStep.NextStep == null ? (int?)null : runnedNextWorkflowStep.NextStep.Id,
-                                                       PreviousStepId = runnedNextWorkflowStep.PreviousStep == null ? (int?)null : runnedNextWorkflowStep.PreviousStep.Id
-                                                   };
+                       {
+                           Id = runnedNextWorkflowStep.Id,
+                           NextStepId = runnedNextWorkflowStep.NextStep == null ? (int?) null : runnedNextWorkflowStep.NextStep.Id,
+                           PreviousStepId =
+                               runnedNextWorkflowStep.PreviousStep == null ? (int?) null : runnedNextWorkflowStep.PreviousStep.Id
+                       };
         }
 
         private RunnedWorkflowStepDto TranslateToDto(RunnedWorkflowStep runnedWorkflowStep)
         {
             return new RunnedWorkflowStepDto
-                            {
-                                Id = runnedWorkflowStep.Id,
-                                RunnedTask = TranslateToDto(runnedWorkflowStep.RunnedTask)
-                            };
+                       {
+                           Id = runnedWorkflowStep.Id,
+                           RunnedTask = TranslateToDto(runnedWorkflowStep.RunnedTask)
+                       };
         }
 
         private RunnedTaskDto TranslateToDto(RunnedTask runnedTask)
@@ -107,7 +123,8 @@ namespace jarvis.server.model
             return runnedWorkflow;
         }
 
-        private void CreateRunnedNextWorkflowSteps(IList<DefinedNextWorkflowStep> nextWorkflowSteps, RunnedWorkflow runnedWorkflow, List<RunnedWorkflowStep> runnedWorkflowSteps)
+        private void CreateRunnedNextWorkflowSteps(IList<DefinedNextWorkflowStep> nextWorkflowSteps, RunnedWorkflow runnedWorkflow,
+                                                   List<RunnedWorkflowStep> runnedWorkflowSteps)
         {
             foreach (var definedNextWorkflowStep in nextWorkflowSteps)
             {
@@ -116,17 +133,23 @@ namespace jarvis.server.model
                 runnedNextWorkflowStep.DefinedNextWorkflowStep = definedNextWorkflowStep;
 
                 if (definedNextWorkflowStep.NextStep == null)
+                {
                     runnedNextWorkflowStep.NextStep = null;
+                }
                 else
                 {
-                    runnedNextWorkflowStep.NextStep = runnedWorkflowSteps.Where(rws => rws.DefinedWorkflowStep == definedNextWorkflowStep.NextStep).Single();
+                    runnedNextWorkflowStep.NextStep =
+                        runnedWorkflowSteps.Where(rws => rws.DefinedWorkflowStep == definedNextWorkflowStep.NextStep).Single();
                 }
 
                 if (definedNextWorkflowStep.PreviousStep == null)
+                {
                     runnedNextWorkflowStep.PreviousStep = null;
+                }
                 else
                 {
-                    runnedNextWorkflowStep.PreviousStep = runnedWorkflowSteps.Where(rws => rws.DefinedWorkflowStep == definedNextWorkflowStep.PreviousStep).Single();
+                    runnedNextWorkflowStep.PreviousStep =
+                        runnedWorkflowSteps.Where(rws => rws.DefinedWorkflowStep == definedNextWorkflowStep.PreviousStep).Single();
                 }
 
 
