a9c21e094bfe26a6f68c4c957978d2506fb069f9
Andreas Willich <sabotageandi@gmail.com>
2012-04-07 17:12:44 +0200

    change windows client to wpf

---
+++ jarvis/Client/Windows/windows/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading;
+using System.Windows;
+using Autofac;
+using jarvis.client.common;
+
+namespace jarvis.client.windows
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private static IContainer _container;
+
+        static void Bootstrap()
+        {
+            var containerBuilder = new ContainerBuilder();
+            containerBuilder.RegisterModule<CommonModule>();
+            containerBuilder.RegisterType<ConfigFileConfiguration>().As<IConfiguration>().SingleInstance();
+
+            _container = containerBuilder.Build();
+        }
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            Bootstrap();
+
+            var client = _container.Resolve<Client>();
+
+            //TODO check if server online
+            Thread.Sleep(30000);
+
+            client.Init();
+
+            var window = new MainWindow();
+            window.Show();
+
+            client.Run();
+        }
+
+    }
+}
